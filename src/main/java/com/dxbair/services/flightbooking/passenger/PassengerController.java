47b8d550f4bf5e4158bdc8c435004fe531d8453d
kishoremarojuu <kishoremarojuu@gmail.com>
2020-04-18 20:51:57 -0500

    first commit

---
+++ src/main/java/com/dxbair/services/flightbooking/passenger/PassengerController.java
@@ -0,0 +1,32 @@
+package com.dxbair.services.flightbooking.passenger;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.dxbair.services.flightbooking.domain.entity.Passenger;
+
+@RestController
+@RequestMapping("passengers")
+public class PassengerController {
+
+	@Autowired
+	private PassengerService passengerService;
+
+	private static final Logger logger = LoggerFactory.getLogger(PassengerController.class);
+
+	@GetMapping
+	public @ResponseBody List<Passenger> getAllPassengers() {
+		return passengerService.getAllPassengers();
+	}
+
+	@GetMapping("/{passenger-id}")
+	public @ResponseBody Passenger getPassengerById(@PathVariable("passenger-id") String passengerId) {
+		return passengerService.getPassengerById(passengerId);
+	}
+
+}
